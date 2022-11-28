d21a21c9a5eaea9310b14386073af6d90927cbb7
JubayerDev <ajashes858@gmail.com>
2022-11-28 16:34:19 +0600

    categories data load

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/categories.json');
 
 
 app.get('/', (req, res) => {
     res.send('Dragon News Server is Serving');
+})
+
+app.get('/news-categories', (req, res) => {
+    res.send(categories);
 })
 
 app.listen(port,() => {
