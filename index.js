188f6a41a5d1741519161f2eb7c917c131179a28
JubayerDev <ajashes858@gmail.com>
2022-11-30 15:04:10 +0600

    news and category api

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./data/categories.json');
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
+const e = require("express");
 
+app.get("/", (req, res) => {
+  res.send("Dragon News Server is Serving");
+});
 
-app.get('/', (req, res) => {
-    res.send('Dragon News Server is Serving');
+app.get("/news-categories", (req, res) => {
+  res.send(categories);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const category_news = news.filter((n) => n.category_id === id);
+    res.send(category_news);
+  }
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+
+app.get('/news', (req, res) => {
+    res.send(news);
 })
 
-app.get('/news-categories', (req, res) => {
-    res.send(categories);
-})
-
-app.listen(port,() => {
-    console.log('Dragon is Running on Port:',port);
-})
+app.listen(port, () => {
+  console.log("Dragon is Running on Port:", port);
+});
